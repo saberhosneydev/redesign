ff94f7e8af7ac4e74dd5c620728c0822697aa36d
saberhosneydev <ssaber277@gmail.com>
2020-01-27 13:57:41 +0200

    Adding scss template for future usage

---
+++ webpack_scss_template/webpack.config.js
@@ -0,0 +1,57 @@
+// Webpack uses this to work with directories
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './src/js/index.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+    {
+      // Apply rule for .sass, .scss or .css files
+      test: /\.(sa|sc|c)ss$/,
+
+      // Set loaders to transform files.
+      // Loaders are applying from right to left(!)
+      // The first loader will be applied after others
+      use: [
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      {
+       // This loader resolves url() and @imports inside CSS
+       loader: "css-loader",
+     },
+     {
+      // Then we apply postCSS fixes like autoprefixer and minifying
+      loader: "postcss-loader"
+    },
+    {
+     // First we transform SASS to standard CSS
+     loader: "sass-loader"
+   }
+   ]
+ }
+ ]},
+ plugins: [
+
+ new MiniCssExtractPlugin({
+  filename: "bundle.css"
+})
+
+ ],
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript
+  // minifying and other thing so let's set mode to development
+  mode: 'production'
+};
