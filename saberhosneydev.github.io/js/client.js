41c97d56fed808cdbf1eee234a6d99a07a4034bd
saberhosneydev <ssaber277@gmail.com>
2020-01-29 22:11:39 +0200

    Finishing up home page & prepare home page animations

--- saberhosneydev.github.io/js/client.js
+++ saberhosneydev.github.io/js/client.js
@@ -1,14 +1,36 @@
-function toggle(){
+function toggleMessage() {
     let currentState;
-    let element = document.getElementsByClassName('hidden');
-    if(element[0].getAttribute('currentState') == "true"){
+    let element = document.getElementsByClassName("hidden");
+    if (element[0].getAttribute("currentState") == "true") {
         currentState = true;
         element[0].style.display = "inline";
-        element[0].setAttribute('currentState', !currentState );
-
-    }else {
+        element[0].setAttribute("currentState", !currentState);
+    } else {
         currentState = false;
         element[0].style.display = "none";
-        element[0].setAttribute('currentState', !currentState);
+        element[0].setAttribute("currentState", !currentState);
+    }
+}
+function toggleContact() {
+    let currentState;
+    let element = document.getElementsByClassName('contact_area');
+    let button = document.getElementsByClassName('contact_me__expander');
+    if (element[0].getAttribute("currentState") == "true") {
+        currentState = true;
+        //Change the button look
+        button[0].classList.add("contact_me__animate");
+        button[0].classList.remove("contact_me__animate__reverse");
+        button[0].style.fill = "white";
+        //Show the contact me section
+        element[0].style.display = "block";
+        element[0].setAttribute("currentState", !currentState);
+    } else {
+        currentState = false;
+        //Change the button look
+        button[0].classList.remove("contact_me__animate");
+        button[0].classList.add("contact_me__animate__reverse");
+        button[0].style.fill = "#8072b3";
+        element[0].style.display = "none";
+        element[0].setAttribute("currentState", !currentState);
     }
 }
