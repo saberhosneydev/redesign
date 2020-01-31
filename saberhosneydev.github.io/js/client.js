4ca5c89ca3f8b9f3955b0e0d16bf1614569fdb61
saberhosneydev <ssaber277@gmail.com>
2020-01-31 15:56:28 +0200

    Finlizing the home page and creat first preview of about page , 50% left

--- saberhosneydev.github.io/js/client.js
+++ saberhosneydev.github.io/js/client.js
@@ -15,19 +15,19 @@ function openContact() {
     let element = document.getElementsByClassName('contact_area');
     let close = document.getElementsByClassName('contact_me__close');
     let open = document.getElementsByClassName('openContact');
-        //Animate the Close button
-        close[0].classList.add('contact_me__close__animate');
-        close[0].classList.remove('contact_me__close__animate__reverse');
-        //Show the contact me section
-        element[0].classList.add("contact_area__animate");
-        element[0].classList.remove("contact_area__animate__reverse");
-        //Close the open button
-        open[0].style.display = "none";
-    }
-    function closeContact() {
-        let element = document.getElementsByClassName('contact_area');
-        let close = document.getElementsByClassName('contact_me__close');
-        let open = document.getElementsByClassName('openContact');
+    //Animate the Close button
+    close[0].classList.add('contact_me__close__animate');
+    close[0].classList.remove('contact_me__close__animate__reverse');
+    //Show the contact me section
+    element[0].classList.add("contact_area__animate");
+    element[0].classList.remove("contact_area__animate__reverse");
+    //Close the open button
+    open[0].style.display = "none";
+}
+function closeContact() {
+    let element = document.getElementsByClassName('contact_area');
+    let close = document.getElementsByClassName('contact_me__close');
+    let open = document.getElementsByClassName('openContact');
     //Animate the Close button
     close[0].classList.remove('contact_me__close__animate');
     close[0].classList.add('contact_me__close__animate__reverse');
@@ -36,17 +36,21 @@ function openContact() {
     element[0].classList.add("contact_area__animate__reverse");
     open[0].style.display = "block";
 }
-function sendEmail(){
-    let guestName = document.getElementById('name');
-    let guestEmail = document.getElementById('email');
-    let content = document.getElementById('content');
-        Email.send({
-            SecureToken : "61e6bdea-a477-4e37-906c-45084d90509f",
-            To : 'eldsitefeedback@gmail.com',
-            From : guestEmail.value,
-            Subject : "SHDev.codes" + "From : " + guestName.value,
-            Body : content.value
-        }).then(
-        message => alert(message)
-        );
-}
+
+document.querySelector("#contactForm").addEventListener("submit", function(event) {
+   event.preventDefault();
+   emailjs.init("user_VV0kGVLqBYwD9xWC73hfK");
+   var template_params = {
+    "Name": document.getElementById('name').value,
+     "Email": document.getElementById('email').value,
+     "Content": document.getElementById('content').value
+ }
+
+ var service_id = "default_service";
+ var template_id = "website_default_form";
+ emailjs.send(service_id, template_id, template_params).then(function(response) {
+       alert('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       alert('FAILED...', error);
+    });;
+}, false);
