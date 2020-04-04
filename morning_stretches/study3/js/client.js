22b912044673eddab91a3a8b6adfd2624c394d8e
SaberHosneyDev <saber20_13@hotmail.com>
2020-04-04 16:08:58 +0200

    moving out to morning stretches

--- morning_stretches/study3/js/client.js
+++
@@ -1,7 +0,0 @@
-let searchIcon = document.getElementById('search-icon');
-let seach = document.getElementById('search');
-searchIcon.addEventListener("click", function() {
-    search.classList.add("openSearch");
-    searchIcon.classList.add("openSearchIcon");
-    search.focus();
-}, false);
